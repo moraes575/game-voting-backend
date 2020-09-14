1a7088a476a27f53b70db864cc9d58ebf11148e0
Matheus Moraes <matheus.moraes111@outlook.com>
2020-09-14 12:50:26 -0300

    Project created

---
+++ src/main/java/com/moraes/gamevotingbackend/GameVotingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moraes.gamevotingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVotingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameVotingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moraes/gamevotingbackend/GameVotingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moraes.gamevotingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVotingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
